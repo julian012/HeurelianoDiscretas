9f079e3b8c4ab218894503b824b21014293aaafd
fabianramos01 <fabian.ramos01@uptc.edu.co>
2019-07-31 15:34:04 -0500

    ....

--- ServerHamiltonianPath/index.js
+++ ServerHamiltonianPath/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 // routes
 var graphs = require('./graph.js');
